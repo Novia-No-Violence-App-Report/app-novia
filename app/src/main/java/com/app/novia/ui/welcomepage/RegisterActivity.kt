e337aee6e089dc97465202941dfe4301dd422c42
farhanderis <farhan.deristianto@gmail.com>
2021-05-26 13:42:36 +0700

    Add activities for Welcoming Page

--- app/src/main/java/com/app/novia/ui/welcomepage/RegisterActivity.kt
+++ app/src/main/java/com/app/novia/ui/welcomepage/RegisterActivity.kt
@@ -1,12 +1,12 @@
-package com.app.novia.ui
+package com.app.novia.ui.welcomepage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.app.novia.R
 
-class SplashScreen : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_register)
     }
 }
---
+++ app/src/main/java/com/app/novia/ui/welcomepage/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.app.novia.ui.welcomepage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.novia.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
