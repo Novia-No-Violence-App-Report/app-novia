a84f053b3f5c31809bff182667415cf73e32348a
farhanderis <farhan.deristianto@gmail.com>
2021-05-29 17:12:25 +0700

    Create register authentication with Firebase

--- app/src/main/java/com/app/novia/ui/welcomepage/RegisterActivity.kt
+++ app/src/main/java/com/app/novia/ui/welcomepage/RegisterActivity.kt
@@ -2,21 +2,65 @@ package com.app.novia.ui.welcomepage
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.app.novia.databinding.ActivityRegisterBinding
 import com.app.novia.ui.mainactivity.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnRegister.setOnClickListener {
-            //TODO: Register logic
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnSignUp.setOnClickListener {
+            val email = binding.txtEmail.text.toString().trim()
+            val password = binding.txtPassword.text.toString().trim()
+
+            if(email.isEmpty()) {
+                binding.txtEmail.error = "E-mail harus diisi!"
+                binding.txtEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.txtEmail.error = "E-mail tidak valid!"
+                binding.txtEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(password.isEmpty() || password.length < 6) {
+                binding.txtPassword.error = "Password harus lebih dari 6 karakter!"
+                binding.txtPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            registerUser(email, password)
         }
     }
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful) {
+                    Intent(this@RegisterActivity, MainActivity::class.java).also {
+                        it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(it)
+                    }
+                }
+                else {
+                    Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
